cf7ef99e82c4aeb4cd7a61d6d4417364c621c942
laurasierra17 <laura.sierra17@gmail.com>
2022-07-05 17:20:50 -0400

    fetch api

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,8 +4,22 @@ function getUserIntput(e) {
     e.preventDefault();
     var userInput = document.getElementById("loc-input").value;
     var optionSelected = document.getElementById("dropdown").value;
+
+    fetchRecords(userInput, optionSelected);
 }
 
 searchResultsBtn.addEventListener("click", getUserIntput);
 
 
+// Call API
+function fetchRecords(userInput, optionSelected) {
+    if (optionSelected) {
+        var url = `https://www.loc.gov/${optionSelected}/?q=${userInput}&fo=json`;
+    } else {
+        var url = `https://www.loc.gov/search/?q=${userInput}&fo=json`;
+    }
+
+    fetch(url)
+    .then(response => response.json())
+    .then(data => console.log(data));
+}
