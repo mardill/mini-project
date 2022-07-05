adb65d5390c01b9a72df75badbc5a45ce74dc149
laurasierra17 <laura.sierra17@gmail.com>
2022-07-05 17:06:33 -0400

    grab user input and format value

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,11 @@
+var searchResultsBtn = document.getElementById("search-results-btn");
+
+function getUserIntput(e) {
+    e.preventDefault();
+    var userInput = document.getElementById("loc-input").value;
+    var optionSelected = document.getElementById("dropdown").value;
+}
+
+searchResultsBtn.addEventListener("click", getUserIntput);
 
 
